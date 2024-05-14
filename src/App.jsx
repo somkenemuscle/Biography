92d2fcad212146f8d9561f025bfe5324d813fbf2
Somkene Ojukwu <somkeneoj@gmail.com>
2024-05-14 16:24:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css';
+import Carousel from './components/carousel';
+import './styles/navbar.css'
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar sticky-top navbar-expand-lg">
+        <div className="container-fluid ">
+          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+
+                <a className="nav-link" aria-current="page" href="#life" >Life</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#education" >Education</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#career">Career</a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#achievements" >
+                  Achievement
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#books">Books</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#nobelprize" >
+                  Nobel prize
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* Carousel component */}
+      <Carousel />
+
+
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/carousel.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styles/carousel.css'
+import albert1 from '../image/chidindu (18).png';
+import albert2 from '../image/work (2).png';
+import albert3 from '../image/chidindu (22).png';
+
+function carousel() {
+    return (
+        <div>
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+
+                    <div className="carousel-item active">
+                        <img src={albert1} class="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={albert2} className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={albert3} className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default carousel
