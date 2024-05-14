af924423c6a678a705a4d9da7e5bf351346bed20
Somkene Ojukwu <somkeneoj@gmail.com>
2024-05-14 21:20:07 +0100

    working on career page

--- src/components/career.jsx
+++ src/components/career.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import '../styles/career.css'
 import career_graph1 from '../image/Mask group (1).png';
-import career_img1 from '../image/Einstein_patentoffice.jpg';
 import career_img2 from '../image/Einstein-with-habicht-and-solovine.jpg';
-import career_img3 from '../image/the newyorktimes.png';
-import career_img4 from '../image/Albert_Einstein_(Nobel).png';
-import career_img5 from '../image/lossy-page1-440px-League_of_Nations_Commission_067.tif.jpg';
-import career_img6 from '../image/1919_eclipse_positive.jpg';
 import career_img7 from '../image/MillikanLemaitreEinstein.jpg';
-import career_img8 from '../image/Albert_Einstein_photo_1920.jpg';
 import career_img9 from '../image/440px-Photoelectric_effect_in_a_solid_-_diagram.svg.png';
-import career_img10 from '../image/Albert_Einstein_1921_(re-cropped).jpg';
 import career_img11 from '../image/anothernews.jpg';
 import career_img12 from '../image/Niels_Bohr_Albert_Einstein4_by_Ehrenfest_cr.jpg';
 import career_img13 from '../image/Solvay_conference_1927.jpg';
